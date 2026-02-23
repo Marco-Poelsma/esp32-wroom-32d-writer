0fba136d444c73d032d7eccd713bfa68525cdd01
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2026-02-23 09:43:35 +0100

    feat: send_message.py, a function to send a message over UDP

--- src/send_message.py
+++ src/send_message.py
@@ -2,9 +2,16 @@ from Message import Message
 import socket
 
 def send_message(message: Message, port: int, ip: str):
+    # We define the socket to use internet and UDP
     sock = socket.socket(socket.AF_INET,
                          socket.SOCK_DGRAM)
-    sock.bind((ip, port))
+    
+    # The data we will send ca be found here
+    data = str(f"{message.email} diu {message.content}").encode('utf-8')
+    
+    # Now we can send the data over UDP.
+    sock.sendto(data, (ip, port))
 
-    while True:
-        
+    sock.close()
+
+    pass
