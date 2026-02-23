cbdc1052a1fc7bed2a19023877237a8c7b52e334
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2026-02-23 09:12:23 +0100

    chore: initial commit

---
+++ Message.py
@@ -0,0 +1,23 @@
+class Message:
+    __email: str = None
+    __content: str = None
+
+    def init(self, email: str, content: str):
+        self.__email = email
+        self.__content = content
+
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, email: str):
+        self.__email = email
+
+    @property
+    def content(self):
+        return self.__content
+    
+    @content.setter
+    def content(self, content: str):
+        self.__content = content
